59b73f83e5087d9d071e4714fb5b042fabbcda19
lior <liorasafov@gmail.com>
2023-04-22 15:10:00 +0300

    updated uploads

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,32 +1,46 @@
 
 
 //!This middleware upload images to public/uploads, its required to be, otherwise cant continue to addProduct function
-const multer = require("multer");
+const path = require('path');
+const express = require('express');
+const multer = require('multer');
 
+const app = express();
 
+// Set up multer storage engine
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-
-   cb(null, "https://surf-shop.onrender.com/public/uploads/");
+    cb(null, path.join(__dirname, 'public', 'uploads'));
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-    cb(null, true);
-  }
-  cb(null, false);
-};
-
+// Set up multer upload middleware
 const upload = multer({
   storage,
   limits: {
     fileSize: 1024 * 1024 * 2,
   },
-  fileFilter,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  },
 });
 
-module.exports = upload;
+// Serve the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for handling file upload
+app.post('/upload', upload.single('image'), (req, res) => {
+  res.json({ filename: req.file.filename });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
