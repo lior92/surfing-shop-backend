87838c5710c86b597333ff8595da83b9ce41cc4d
lior <liorasafov@gmail.com>
2023-04-22 15:15:23 +0300

    upload0.2

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,15 +1,12 @@
 
 
 //!This middleware upload images to public/uploads, its required to be, otherwise cant continue to addProduct function
-const path = require('path');
-const express = require('express');
-const multer = require('multer');
+const multer = require("multer");
 
-const app = express();
 
-// Set up multer storage engine
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
+
     cb(null, path.join(__dirname, 'public', 'uploads'));
   },
   filename: (req, file, cb) => {
@@ -17,30 +14,19 @@ const storage = multer.diskStorage({
   },
 });
 
-// Set up multer upload middleware
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+    cb(null, true);
+  }
+  cb(null, false);
+};
+
 const upload = multer({
   storage,
   limits: {
     fileSize: 1024 * 1024 * 2,
   },
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
-      cb(null, true);
-    } else {
-      cb(null, false);
-    }
-  },
+  fileFilter,
 });
 
-// Serve the public directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Route for handling file upload
-app.post('/upload', upload.single('image'), (req, res) => {
-  res.json({ filename: req.file.filename });
-});
-
-// Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+module.exports = upload;
