6f4420610d5317d1a3d09b56931336477a07c0de
lior <liorasafov@gmail.com>
2023-04-23 02:19:12 +0300

    c

--- middleware/upload.js
+++ middleware/upload.js
@@ -6,7 +6,7 @@ const multer = require("multer");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
 
-    cb(null, "https://surf-shop.onrender.com/public/uploads/");
+    cb(null, "public/uploads/");
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
