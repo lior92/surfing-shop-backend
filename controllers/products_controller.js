f4ebb99bca630827801173e2f563745b4f23a03b
lior <liorasafov@gmail.com>
2023-04-23 16:31:22 +0300

    2.3

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -164,41 +164,35 @@ return res.status(200).json({
     }
  },
 
+ deleteProduct:async (req, res) => {
+  try {
+    const product_id = req.params.product_id;
+    const product = await Product.findById(product_id);
 
+    if (!product) {
+      throw new Error("Product not found");
+    }
 
- // ...
- 
- deleteProduct: async (req, res) => {
-   try {
-     const product_id = req.params.product_id;
-     const product = await Product.findById(product_id);
-     if (!product) {
-       throw new Error("Product not found");
-     }
- 
-     // Delete the product from the database
-     await Product.findByIdAndDelete(product_id);
- 
-     // Delete the image file from the uploads folder
-     const imagePath = path.join(__dirname, "../uploads", product.image);
-     fs.unlink(imagePath, (err) => {
-       if (err) console.error(err);
-       console.log(`Deleted file: ${imagePath}`);
-     });
- 
-     return res.status(200).json({
-       success: true,
-       message: "Success to delete product",
-     });
-   } catch (error) {
-     console.error(error);
-     return res.status(500).json({
-       message: "Error in delete product request",
-       error: error.message,
-     });
-   }
- }
- 
+    // Delete the product image from the uploads folder
+    const imagePath = path.join(__dirname, "../uploads", product.product_image);
+    fs.unlinkSync(imagePath);
+
+    // Delete the product from the database
+    await Product.findByIdAndDelete(product_id);
+
+    return res.status(200).json({
+      success: true,
+      message: "Success to delete product",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Error in delete product request",
+      error: error.message,
+    });
+  }
+}
+
 
 
 
